fef81dd1bbde3c2d4f9e24fb61fef7839d93cc70
Tobias Leigh-Wood <tjleighwood@uchicago.edu>
2024-05-19 11:03:30 -0500

    Plot cumlative distance and find fit files we need

--- 2__explore.R
+++ 2__explore.R
@@ -1,17 +1,42 @@
 library(data.table)
-
-dir()
+library(ggplot2)
 
 activity_dt <- fread("strava_data/activities.csv") |>
     setnames(tolower) |>
     setnames(\(x) gsub(" ", "_", x))
-
-activity_dt[, activate_date := as.IDateTime(activate_date)]
 
 activity_dt[, activity_day := as.IDate(
     gsub(",.+$", "", activity_date),
     format = "%d %b %Y"
 )]
 
+istanbul_trip <- activity_dt[activity_day >= "2022-06-25" &
+    activity_day <= "2022-08-05"]
 
-activity_dt[, head(activity_day)]
+
+istanbul_trip[, sum(distance)]
+istanbul_trip[, sum(elevation_gain)]
+
+istanbul_trip[distance < 0]
+istanbul_trip[
+    , .(daily_distance = sum(distance)),
+    activity_day
+] |>
+    _[order(activity_day), cumlative_distance := cumsum(daily_distance)] |>
+    ggplot(
+        aes(
+            x = activity_day,
+            y = cumlative_distance
+        )
+    ) +
+    geom_point() +
+    geom_line() +
+    theme_minimal() +
+    labs(
+        x = "Date",
+        y = "Kilometers",
+        title = "Istanbul trip daily distance"
+    )
+
+
+gpx_files_to_get <- istanbul_trip[, filename]
